5984fe12a5b49bcba1ab214298ba3edafb626fe1
DeutscheMark <deutschemark88@gmail.com>
2022-04-15 17:20:21 +0200

    connection.ts: Update command for got

--- src/util/connection.ts
+++ src/util/connection.ts
@@ -4,7 +4,7 @@ import { PlatformConfig } from 'homebridge';
 import got from 'got';
 
 export = {
-    
+
     getOptions: function (config: PlatformConfig, method: string, params: any) {
         const host = config.host || 'localhost';
         const port = config.port || '8080';
@@ -20,7 +20,7 @@ export = {
 
     kodiRequest: async function (config: PlatformConfig, method: string, params: any) {
         const request = async function (options: any) {
-            const response = await got.post(options.uri, { json: options.json, timeout: 1000, retry: 2 }).json() as any;
+            const response = await got.post(options.uri, { json: options.json, timeout: { request: 1000 }, retry: { limit: 2 } }).json() as any;
             return (response.result as any);
         };
         return await request(this.getOptions(config, method, params));
